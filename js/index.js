3b1f776edf62e3035e609988341b3ec2aedf15de
kimddddh98 <amawang11@gmail.com>
2022-08-15 03:15:19 +0900

    slide animate

--- js/index.js
+++ js/index.js
@@ -1,8 +1,58 @@
 $(function(){
-        full.style.width = document.getElementById('full').clientWidth * $('#full>div').length + 'px';
-        const offset2 = $('#full2').offset().left
-        let index=1;
-       
+    
+    $('.circleItem').on('click', function () {
+        let circleIndex = $(this).index();
+        $(this).siblings().hide();
+        $(this).animate({ scale: '700%' }, 1000, function () {
+            $(this).css({ transform: 'scale(100%)' });
+            $('#container').hide();
+            $('#full').css({
+                display: 'flex',
+                width: 100 * $('#full>div').length + 'vw'
+            });
+            const offset2 = $('#full2').offset().left
+            $('#full').css({ left: -offset2 * circleIndex })
+            console.log(offset2)
+            let index = circleIndex + 1;
+
+            $('#full').on("mousewheel", function (event) {
+
+                if (event.originalEvent.wheelDelta <= 0) {
+                    if (index != $('#full>div').length) {
+                        $('#full>div').stop().animate({
+                            height: 90 + 'vh',
+                            width: 90 + 'vw'
+                        }, function () {
+                            $('#full').stop().animate({ left: -offset2 * index }, function () {
+                                $('#full>div').stop().animate({
+                                    height: 100 + '%',
+                                    width: 100 + 'vw'
+                                })
+                                index++
+                            })
+                        })
+                    };
+                }
+                else {
+                    if (index != 1) {
+                        $('#full>div').stop().animate({
+                            height: 90 + 'vh',
+                            width: 90 + 'vw'
+                        }, function () {
+                            $('#full').stop().animate({ left: -offset2 * (index - 2) }, function () {
+                                index--
+                                $('#full>div').stop().animate({
+                                    height: 100 + '%',
+                                    width: 100 + 'vw'
+                                })
+                            })
+                        })
+                    };
+                }
+            })
+
+        })
+    })
     $('#container').on('mousewheel', function (e) {
         let circleArr = [];
         let found = /-\d+|\d+/g;
@@ -27,47 +77,6 @@ $(function(){
         };
     })
 
-        $(window).on("mousewheel", function (event) {
-           
-            if (event.originalEvent.wheelDelta <= 0) {
-                if(index==$('#full>div').length){
-                    index=index
-                }
-                
-                else {
-                    $('#full>div').stop().animate({
-                        height: 90 + 'vh',
-                        width: 90 + 'vw'
-                    },function(){
-                        $('#full').stop().animate({ left: -offset2*index },function(){
-                            $('#full>div').stop().animate({
-                            height: 100 + '%',
-                            width: 100 + 'vw'
-                            })
-                            index++
-                        })
-                    })
-                };
-            }
-            else {
-
-                if(index==1){index=index}
-                
-                else {
-                    $('#full>div').stop().animate({
-                        height: 90 + 'vh',
-                        width: 90 + 'vw'
-                    },function(){
-                        $('#full').stop().animate({ left: -offset2*(index-2) },function(){
-                            index--
-                            $('#full>div').stop().animate({
-                            height: 100 + '%',
-                            width: 100 + 'vw'
-                            })
-                        })
-                    })
-                };
-            }
-        })
+        
         
 });
