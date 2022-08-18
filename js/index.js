e8abcc87ef69f77e55305af38bbc8dd08fff0caf
kimddddh98 <104114436+kimddddh98@users.noreply.github.com>
2022-08-18 18:05:28 +0900

    menu slide

--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
 window.onload = () => {
+    var cmp =[
+        {template: '<div><div v-for="(item,index) in circleStyle">지역 컴포넌트가 등록되었습니다{{index}}</div></div>'},
+        {template: '<div>지역 컴포넌트가 등록되었습니다{{index}}</div>'},
+        {template: '<div>지역 컴포넌트가 등록되었습니다3</div>'},
+        {template: '<div>지역 컴포넌트가 등록되었습니다4</div>'},
+        {template: '<div>지역 컴포넌트가 등록되었습니다5</div>'}
+    ]
     new Vue({
         el: '#app',
         data: {
@@ -11,10 +18,16 @@ window.onload = () => {
                 { transform: "rotateY(210deg) translateZ(30vw) translateY(30vw)" },
                 { transform: "rotateY(280deg) translateZ(30vw) translateY(40vw)" },
             ],
-            click: 0
+            click: 0,
+            header:false
+            
         },
+        // components:{
+        //     "my-component":cmp[1]
+        // },
         methods: {
             subPage: function (e) {
+                this.header=true
                 let headerMenu=document.querySelectorAll('.menuColor')
                 this.main = false;
                 this.sub = true
@@ -37,13 +50,37 @@ window.onload = () => {
                   
                 }
             },
+            circleOver(e){
+                // let found = /-\d+|\d+/g;
+                // let transfromArr=[]
+                // let circleItem=document.querySelectorAll('.circleItem')
+
+                // for(let i in this.circleStyle){
+                //     transfromArr.push(circleItem[i].style.transform.match(found))
+
+                // }
+
+                // circleItem[0].style.transform=`rotateY(${parseInt(transfromArr[0][0])}deg) translateZ(${parseInt(transfromArr[0][1])}vw) translateY(${parseInt(transfromArr[0][2])}vw) scale(110%)`
+                
+            },
+            circleOut(e){
+                // let found = /-\d+|\d+/g;
+                // let transfromArr=[]
+                // let circleItem=document.querySelectorAll('.circleItem')
+
+                // for(let i in this.circleStyle){
+                //     transfromArr.push(circleItem[i].style.transform.match(found))
+                // }
+                // circleItem[0].style.transform=`rotateY(${parseInt(transfromArr[0][0])}deg) translateZ(${parseInt(transfromArr[0][1])}vw) translateY(${parseInt(transfromArr[0][2])}vw)`
+
+
+            },
             mainWheel: function (e) {
                 let found = /-\d+|\d+/g;
                 let circleItem = document.getElementById('menu').children;
                 let circleArr = [];
 
                 if (e.wheelDelta <= 0) {
-
                     if (circleItem[0].style.transform != 'rotateY(-280deg) translateZ(30vw) translateY(-40vw)') {
                         for (let i = 0; i < circleItem.length; i++) {
                             circleArr.push(circleItem[i].style.transform.match(found))
@@ -70,18 +107,24 @@ window.onload = () => {
                         const fullbox = document.querySelectorAll('.full');
                         const full = document.getElementById('full');
                         let headerMenu=document.querySelectorAll('.menuColor')
-                        for(let h=0;h<headerMenu.length;h++){
-                            headerMenu[h].style.color='#DCE2F0'    
-                        }
+
                         if (e.wheelDelta <= 0) {
                             if (this.click != fullbox.length - 1 &&this.click <fullbox.length - 1 ) {
                                 for (let i = 0; i < fullbox.length; i++) {
                                     fullbox[i].style.width = 90 + 'vw'
                                     fullbox[i].style.height = 85 + 'vh'
                                 }
-                                this.click==fullbox.length
-                            headerMenu[this.click+2].style.color='red'
+                                    this.click==fullbox.length
+                                    if(this.click<headerMenu.length-2){
+                                        for(let h=0;h<headerMenu.length;h++){
+                                            headerMenu[h].style.color='#DCE2F0'    
+                                        }
+                                        headerMenu[this.click+2].style.color='red'
+                                        
+                                    }
 
+
+                                
                                 full.style.left = -100 * (this.click + 1) + 'vw'
                                 setTimeout(() => {
                                     for (let i = 0; i < fullbox.length; i++) {
@@ -98,8 +141,14 @@ window.onload = () => {
                                     fullbox[i].style.width = 90 + 'vw'
                                     fullbox[i].style.height = 85 + 'vh'
                                 }
-                                headerMenu[this.click].style.color='red'
-
+                                // headerMenu[this.click].style.color='red'
+                                // if(this.click<headerMenu.length-2){
+                                    for(let h=0;h<headerMenu.length;h++){
+                                        headerMenu[h].style.color='#DCE2F0'    
+                                    }
+                                    headerMenu[this.click].style.color='red'
+                                    
+                                // }
                                 full.style.left = -100 * (this.click - 1) + 'vw'
                                 setTimeout(() => {
                                     for (let i = 0; i < fullbox.length; i++) {
@@ -115,6 +164,7 @@ window.onload = () => {
                 }
             },
             home(e){
+                this.header=false
                 let headerMenu=document.querySelectorAll('.menuColor')
                 for(let h=0;h<headerMenu.length;h++){
                     headerMenu[h].style.color='#DCE2F0'    
@@ -122,7 +172,37 @@ window.onload = () => {
                 e.target.style.color='red'
                 this.main = true;
                 this.sub = false
+            },
+            section(e){
+                console.log(this.click)
+                let full=document.getElementById('full')
+                let fullbox=document.querySelectorAll('.full')
+                let headerMenu=document.querySelectorAll('.menuColor')
+                // headerMenu[this.click].style.color='red'
+                for(let h=0;h<headerMenu.length;h++){
+                    headerMenu[h].style.color='#DCE2F0' 
+                    e.target.style.color='red'
+                    if(headerMenu[h].style.color=='red') 
+                    this.click=h-1 
+                    
+
+                }
+                for (let i = 0; i < fullbox.length; i++) {
+                    fullbox[i].style.width = 90 + 'vw'
+                    fullbox[i].style.height = 85 + 'vh'
+                }
+                full.style.left=-100*this.click+'vw'
+
+                setTimeout(()=>{
+                for (let i = 0; i < fullbox.length; i++) {
+                    fullbox[i].style.width = 100+ 'vw'
+                    fullbox[i].style.height = 100 + 'vh'
+                }
+
+                },600)
+                // return this.click=2
             }
         }
+       
     })
 }
